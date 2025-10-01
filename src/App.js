1c8539b1a8fec23f54728b4d386f5a152d793bd6
Abdul Muthalib Mohammed <abdulmuthalibmohammed18@gmail.com>
2025-10-01 14:00:55 +0000

    Revert "Merge branch 'update/vitals-zod' into 'develop'"
    This reverts merge request !50

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./Components/Navbar";
 import Dashboard from './Pages/Dashboard';
 import PatientRegistration from './Pages/PatientRegistration';
@@ -42,7 +40,6 @@ function App() {
     <div className="app-container">
       <Router>
         <Navbar />
-        <ToastContainer />
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Login />} />
