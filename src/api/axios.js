289d283b71853e18bd3cbefacd6c90981473cd77
DIVYANSH-TEJA-09 <143836807+DIVYANSH-TEJA-09@users.noreply.github.com>
2025-11-30 17:17:29 +0530

    Updated server and axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const BACKEND_URL = 'https://be-medical-camp.apps.swecha.org';
-const BACKEND_URL = 'http://localhost:5002';
+const BACKEND_URL = 'https://be-medical-camp.apps.swecha.org';
+//const BACKEND_URL = 'http://localhost:5002';
 
 const publicAxios = axios.create({
     baseURL: BACKEND_URL,
@@ -22,3 +22,4 @@ privateAxios.interceptors.request.use(
 );
 
 export { privateAxios, publicAxios };
+
