7ac0213a2127097c669946e3355beae7c09ed8dd
DIVYANSH-TEJA-09 <143836807+DIVYANSH-TEJA-09@users.noreply.github.com>
2025-11-30 16:51:07 +0530

    VolunteerManual and Patient Validation

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BACKEND_URL = 'https://be-medical-camp.apps.swecha.org';
-//const BACKEND_URL = 'http://localhost:5002';
+// const BACKEND_URL = 'https://be-medical-camp.apps.swecha.org';
+const BACKEND_URL = 'http://localhost:5002';
 
 const publicAxios = axios.create({
     baseURL: BACKEND_URL,
@@ -21,4 +21,4 @@ privateAxios.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
-export { publicAxios, privateAxios };
+export { privateAxios, publicAxios };
